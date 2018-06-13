78983809a31ccc9c25358d8d896a5850ad09c5b8
דורון לעדן <laadan@auth.ad.bgu.ac.il>
2018-06-13 21:43:27 +0300

    poi

--- Components/POI/poiController.js
+++ Components/POI/poiController.js
@@ -1,18 +1,13 @@
 angular.module('poiApp')
-.controller('homeController', ['$http', function($http) {
+.controller('poiController', ['$http', function($http) {
     self = this;
 
     let serverUrl = 'http://localhost:3000/';
-    self.user = {
-        username: self.username,
-        password: self.password,
-        isAdmin: self.isAdmin
-    }
 
 
     $http.get(serverUrl + "poi/AllPointsOfInterst")
     .then(function(response){
-        self.recomendedPOI = response.data;
+        self.pois = response.data;
     },function(response){
         self.recomendedPOI = [];
     })
