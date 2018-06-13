035e91c0f1a59e6442d9edb507126f9aabfedfaf
דורון לעדן <laadan@auth.ad.bgu.ac.il>
2018-06-13 14:14:18 +0300

    merge

--- Components/Register/registerController.js
+++ Components/Register/registerController.js
@@ -1,51 +1,70 @@
-angular.module('poiApp')
+angular.module('poiApp'/*, ["checklist-model"]*/)
 .controller('registerController',['$http',  function($http){
-    let serverUrl = 'http://localhost:8080/';
+    let serverUrl = 'http://localhost:3000';
 
     self = this;
-    let user = {
+
+    self.chosenCategories = [];
+
+    self.user = {
         Firstname: self.Firstname,
         Lastname: self.Lastname,
         City: self.City,
         Country: self.Country,
         Email: self.Email,
-        Categories: self.Categories,
-        Category2: self.Category2,
-        Category3: self.Category3,
-        Category4: self.Category4,
+        Category1: null,
+        Category2: null,
+        Category3: null,
+        Category4: null,
         QuestId1: self.QuestId1,
         QuestId2: self.QuestId2,
         Ans1: self.Ans1,
         Ans2: self.Ans1
     }
 
+    $http.get(serverUrl + "/Countries")
+    .then(function(response){
+        self.countries = response.data;
+    },function(response){
+        self.countries = [];
+    })
+
+    $http.get(serverUrl + "/Categories")
+    .then(function(response){
+        self.categories = response.data;
+    },function(response){
+        self.categories = [];
+    })
+
     
-    self.getCountries = function(){
-        return $http.get(serverUrl + "/Countries")
-    }
+    $http.get(serverUrl + "/Questions")
+    .then(function(response){
+        self.questions = response.data;
+    },function(response){
+        self.questions = [];
+    })
 
-    self.getCategories = function(){
-        return $http.get(serverUrl + "/Categories")
-    }
+    self.quesFilter = function (item) { 
+        return true; 
+    };
 
-    self.getQuestions = function(){
-        return $http.get(serverUrl + "/Questions")
-    }
-
-    //let countries = self.getCountries();
-    let countries = ['a', 'b', 'c'];
-    //let questions = self.getQuestions();
-    //let categories = self.getCategories();
 
     self.register = function(){
+        self.user.Category1 = self.chosenCategories[0];
+        self.user.Category2 = self.chosenCategories[1];
+        if(self.chosenCategories.length > 2)
+            self.user.Category3 = self.chosenCategories[2];
+        if(self.chosenCategories.length > 3)
+            self.user.Category4 = self.chosenCategories[3];
+
          // register user
-         $http.post(serverUrl + "Users/register", user)
+         $http.post(serverUrl + "/users/register", self.user)
          .then(function (response) {
              //First function handles success
-             self.signUp.content = response.data;
+             self.register.content = response.data;
          }, function (response) {
              //Second function handles error
-             self.signUp.content = "Something went wrong";
+             self.register.content = "Something went wrong";
          });
     }
 
