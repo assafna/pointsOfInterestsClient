e4681d5036acdb4e9ec614c7cb8c1cd9d85c1929
Assaf <nahum.assaf@gmail.com>
2018-06-18 18:47:00 +0300

    added styling and organizd some code

--- app.js
+++ app.js
@@ -1,42 +1,42 @@
-let app = angular.module('poiApp', ['ngRoute','LocalStorageModule']);
+let app = angular.module('poiApp', ['ngRoute', 'LocalStorageModule']);
 
-app.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+app.config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
     $locationProvider.hashPrefix('');
 
     $routeProvider
         // not registered
         .when('/', {
             templateUrl: 'Components/Login/login.html',
-            controller : 'loginController as lgnCtrl'
+            controller: 'loginController as lgnCtrl'
         })
         .when('/login', {
             templateUrl: 'Components/Login/login.html',
-            controller : 'loginController as lgnCtrl'
+            controller: 'loginController as lgnCtrl'
         })
         // registered
         .when('/home', {
             templateUrl: 'Components/Home/home.html',
-            controller : 'homeController as homeCtrl'
+            controller: 'homeController as homeCtrl'
         })
         .when('/about', {
             templateUrl: 'Components/About/about.html',
-            controller : 'aboutController as abtCtrl'
+            controller: 'aboutController as abtCtrl'
         })
-        .when('/sign in', {
+        .when('/signUp', {
             templateUrl: 'Components/Register/register.html',
-            controller : 'registerController as regCtrl'
+            controller: 'registerController as regCtrl'
         })
         .when('/poi', {
             templateUrl: 'Components/POI/poi.html',
-            controller : 'poiController as poiCtrl'
+            controller: 'poiController as poiCtrl'
         })
         .when('/favPoi', {
             templateUrl: 'Components/FavPOI/favPoi.html',
-            controller : 'favouritePoiController as fpoiCtrl'
+            controller: 'favouritePoiController as fpoiCtrl'
         })
         .otherwise({ redirectTo: '/' });
 }])
-
-.config(function(localStorageServiceProvider){
-    localStorageServiceProvider.setPrefix('poiApp');
-});
+    .config(function (localStorageServiceProvider) {
+        localStorageServiceProvider.setPrefix('poiApp');
+    }
+);
--- indexController.js
+++ indexController.js
@@ -1,70 +1,77 @@
 angular.module('poiApp')
-    .controller('indexController',['setHeadersToken', 'localStorageService', '$window', 'poiDetails','$http','checkTokenValidation', 'initUserInLocalStorage','$scope',
-     function (setHeadersToken, localStorageService, $window, poiDetails, $http, checkTokenValidation, initUserInLocalStorage, $scope) {
+    .controller('indexController', ['setHeadersToken', 'localStorageService', '$window', 'poiDetails', '$http', 'checkTokenValidation', 'initUserInLocalStorage', '$scope',
+        function (setHeadersToken, localStorageService, $window, poiDetails, $http, checkTokenValidation, initUserInLocalStorage, $scope) {
 
-        let serverUrl = 'http://localhost:3000/';
+            let serverUrl = 'http://localhost:3000/';
 
-        self = this;
-        $scope.userName = localStorageService.get('username');
-        $scope.poiToShow = {};
-        $scope.loggedIn = checkTokenValidation.check();
-
-
-        $http.get(serverUrl + "poi/AllPointsOfInterst")
-        .then(function(response){
-            localStorageService.set('allPOI', response.data);
-        },function(response){
-            self.pois = [];
-        }) 
-
-        self.showPoiDetails = function(id){
-            let pois = localStorageService.get('allPOI')
-            // $http.get(serverUrl + "poi/AllPointsOfInterst")
-            // .then(function(response){
-            //     pois = response.data;
-            // },function(response){
-            //     pois = [];
-            // }) 
-            for(let i = 0; i < pois.length; i++){
-                if(pois[i].poiInfo.POI_id == id){
-                    $scope.poiToShow = pois[i];
-                    $scope.poiToShow.description = pois[i].poiInfo.POI_description;
-                    $scope.poiToShow.avgRank = pois[i].poiInfo.POI_angRank;
-                    $scope.poiToShow.numOfViewrs = pois[i].poiInfo.NumOfViewers;
-                    $scope.poiToShow.reviews = pois[i].poiInfo.poiReview;
-                    break;
-                }
-            }
-            document.getElementById("poiDialog").showModal();
-           //add 1 to num of viewers
-            $http.put(serverUrl + "poi/updateNumberOfViewers", { poiId: self.poiToShow.poiInfo.POI_id})
-            .then(function(response){
-                console.log(response)
-            }, function(response){
-                console.log("error");
-            })
-
-        }
-
-        self.closeDialog = function(){
-            document.getElementById("poiDialog").close();
-        }
-
-        $scope.logout = function(){
-            initUserInLocalStorage.deleteUser();
-            $scope.loggedIn = false;
+            self = this;
             $scope.userName = localStorageService.get('username');
-            $window.location.href = '#/login';
-            
-         }
-
-         $scope.checkLogin = function(){
+            $scope.poiToShow = {};
             $scope.loggedIn = checkTokenValidation.check();
-            $scope.userName = localStorageService.get('username');
-            if($scope.loggedIn)
-                setHeadersToken.set(localStorageService.get('token'));
-            return $scope.loggedIn;
 
-         }
+            // get all pois from the server, insert into local storage
+            $http.get(serverUrl + "poi/AllPointsOfInterst")
+                .then(function (response) {
+                    localStorageService.set('allPOI', response.data);
+                }, function (response) {
+                    self.pois = [];
+                    console.log(response);
+                })
 
-    }]);
+            // retrieve relevant poi from local storage according to id
+            // increase the views of the poi by one
+            self.showPoiDetails = function (id) {
+                let pois = localStorageService.get('allPOI')
+                // $http.get(serverUrl + "poi/AllPointsOfInterst")
+                // .then(function(response){
+                //     pois = response.data;
+                // },function(response){
+                //     pois = [];
+                // }) 
+                for (let i = 0; i < pois.length; i++) {
+                    if (pois[i].poiInfo.POI_id == id) {
+                        $scope.poiToShow = pois[i];
+                        $scope.poiToShow.description = pois[i].poiInfo.POI_description;
+                        $scope.poiToShow.avgRank = pois[i].poiInfo.POI_angRank;
+                        $scope.poiToShow.numOfViewrs = pois[i].poiInfo.NumOfViewers;
+                        $scope.poiToShow.reviews = pois[i].poiInfo.poiReview;
+                        break;
+                    }
+                }
+                document.getElementById("poiDialog").showModal();
+                //add 1 to num of viewers
+                $http.put(serverUrl + "poi/updateNumberOfViewers", { poiId: self.poiToShow.poiInfo.POI_id })
+                    .then(function (response) {
+                        console.log(response)
+                    }, function (response) {
+                        console.log("error: " + response);
+                    })
+
+            }
+
+            // closing the dialog of a poi
+            self.closeDialog = function () {
+                document.getElementById("poiDialog").close();
+            }
+
+            // logging out of the system by deleting him from the local storage
+            $scope.logout = function () {
+                initUserInLocalStorage.deleteUser();
+                $scope.loggedIn = false;
+                $scope.userName = localStorageService.get('username');
+                $window.location.href = '#/login';
+            }
+
+            // checks if a user is logged in
+            $scope.checkLogin = function () {
+                $scope.loggedIn = checkTokenValidation.check();
+                $scope.userName = localStorageService.get('username');
+                if ($scope.loggedIn)
+                    setHeadersToken.set(localStorageService.get('token'));
+                return $scope.loggedIn;
+
+            }
+
+        }
+    ]
+);
